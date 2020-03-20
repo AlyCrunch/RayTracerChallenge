d85e8039ba859fb6027a6d50e695896267d8a159
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-03-20 15:57:44 +0100

    Shearing

--- RayTracerChallenge/Helpers/Transformations.cs
+++ RayTracerChallenge/Helpers/Transformations.cs
@@ -34,5 +34,45 @@ namespace RayTracerChallenge.Helpers
 
             return m;
         }
+
+        public static Matrix RotationY(double rotation)
+        {
+            var m = Matrix.GetIdentity(4, 4);
+
+            m[0, 0] = Math.Cos(rotation);
+            m[0, 2] = Math.Sin(rotation);
+            m[2, 0] = -Math.Sin(rotation);
+            m[2, 2] = Math.Cos(rotation);
+
+            return m;
+        }
+
+        public static Matrix RotationZ(double rotation)
+        {
+            var m = Matrix.GetIdentity(4, 4);
+
+            m[0, 0] = Math.Cos(rotation);
+            m[0, 1] = -Math.Sin(rotation);
+            m[1, 0] = Math.Sin(rotation);
+            m[1, 1] = Math.Cos(rotation);
+
+            return m;
+        }
+
+        public static Matrix Shearing(double x1, double x2, double y1, double y2, double z1, double z2)
+        {
+
+            var m = Matrix.GetIdentity(4, 4);
+
+            m[0, 1] = x1;
+            m[0, 2] = x2;
+            m[1, 0] = y1;
+            m[1, 2] = y2;
+            m[2, 0] = z1;
+            m[2, 1] = z2;
+
+            return m;
+        }
+
     }
 }
--- Tests.RTC/MatriceTransformation.cs
+++ Tests.RTC/MatriceTransformation.cs
@@ -8,6 +8,7 @@ namespace Tests.RTC
 {
     public class MatriceTransformation
     {
+        #region Translation
         [Fact]
         public void MultiplyingTranslationMatrix()
         {
@@ -35,7 +36,9 @@ namespace Tests.RTC
 
             Assert.Equal(v, transform * v);
         }
+        #endregion
 
+        #region Scaling
         [Fact]
         public void ScalingMatrixPoint()
         {
@@ -75,7 +78,9 @@ namespace Tests.RTC
 
             Assert.Equal(expected, transform * p);
         }
+        #endregion
 
+        #region Rotation
         [Fact]
         public void RotationPointXAxis()
         {
@@ -89,5 +94,91 @@ namespace Tests.RTC
             var expected2 = RTF.PointType.Point(0, 0, 1);
             CustomAssert.Equal(expected2, fullQuarter * p, 0);
         }
+        [Fact]
+        public void InvertedRotationPointXAxis()
+        {
+            var p = RTF.PointType.Point(0, 1, 0);
+
+            var halfQuarter = RTH.Transformations.RotationX(Math.PI / 4);
+            var inverted = halfQuarter.Inverse();
+
+            var expected = RTF.PointType.Point(0, Math.Sqrt(2) / 2, -Math.Sqrt(2) / 2);
+            CustomAssert.Equal(expected, inverted * p, 5);
+        }
+        [Fact]
+        public void RotationPointYAxis()
+        {
+            var p = RTF.PointType.Point(0, 0, 1);
+
+            var halfQuarter = RTH.Transformations.RotationY(Math.PI / 4);
+            var expected1 = RTF.PointType.Point(Math.Sqrt(2) / 2, 0, Math.Sqrt(2) / 2);
+            Assert.Equal(expected1, halfQuarter * p);
+
+            var fullQuarter = RTH.Transformations.RotationY(Math.PI / 2);
+            var expected2 = RTF.PointType.Point(1, 0, 0);
+            CustomAssert.Equal(expected2, fullQuarter * p, 0);
+        }
+        [Fact]
+        public void RotationPointZAxis()
+        {
+            var p = RTF.PointType.Point(0, 1, 0);
+
+            var halfQuarter = RTH.Transformations.RotationZ(Math.PI / 4);
+            var expected1 = RTF.PointType.Point(-Math.Sqrt(2) / 2, Math.Sqrt(2) / 2, 0);
+            Assert.Equal(expected1, halfQuarter * p);
+
+            var fullQuarter = RTH.Transformations.RotationZ(Math.PI / 2);
+            var expected2 = RTF.PointType.Point(-1, 0, 0);
+            CustomAssert.Equal(expected2, fullQuarter * p, 0);
+        }
+        #endregion
+
+        #region Shearing
+        [Fact]
+        public void ShearingXtoZ()
+        {
+            var transform = RTH.Transformations.Shearing(0, 1, 0, 0, 0, 0);
+            var p = RTF.PointType.Point(2, 3, 4);
+            var expected = RTF.PointType.Point(6, 3, 4);
+
+            Assert.Equal(expected, transform * p);
+        }
+        [Fact]
+        public void ShearingYtoX()
+        {
+            var transform = RTH.Transformations.Shearing(0, 0, 1, 0, 0, 0);
+            var p = RTF.PointType.Point(2, 3, 4);
+            var expected = RTF.PointType.Point(2, 5, 4);
+
+            Assert.Equal(expected, transform * p);
+        }
+        [Fact]
+        public void ShearingYtoZ()
+        {
+            var transform = RTH.Transformations.Shearing(0, 0, 0, 1, 0, 0);
+            var p = RTF.PointType.Point(2, 3, 4);
+            var expected = RTF.PointType.Point(2, 7, 4);
+
+            Assert.Equal(expected, transform * p);
+        }
+        [Fact]
+        public void ShearingZtoX()
+        {
+            var transform = RTH.Transformations.Shearing(0, 0, 0, 0, 1, 0);
+            var p = RTF.PointType.Point(2, 3, 4);
+            var expected = RTF.PointType.Point(2, 3, 6);
+
+            Assert.Equal(expected, transform * p);
+        }
+        [Fact]
+        public void ShearingZtoY()
+        {
+            var transform = RTH.Transformations.Shearing(0, 0, 0, 0, 0, 1);
+            var p = RTF.PointType.Point(2, 3, 4);
+            var expected = RTF.PointType.Point(2, 3, 7);
+
+            Assert.Equal(expected, transform * p);
+        }
+        #endregion
     }
 }
