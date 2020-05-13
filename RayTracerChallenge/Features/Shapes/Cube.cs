dae0dd5813968ec0070a3c0498f20a620679f8e6
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-05-13 15:30:32 +0200

    Chapter 12 - Cubes

---
+++ RayTracerChallenge/Features/Shapes/Cube.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracerChallenge.Features.Shapes
+{
+    public class Cube : Shape
+    {
+        const double EPSILON = 0.00001;
+
+        protected override Intersection[] LocalIntersect(Ray localRay)
+        {
+            (var xtmin, var xtmax) = CheckAxis(localRay.Origin.X, localRay.Direction.X);
+            (var ytmin, var ytmax) = CheckAxis(localRay.Origin.Y, localRay.Direction.Y);
+            (var ztmin, var ztmax) = CheckAxis(localRay.Origin.Z, localRay.Direction.Z);
+
+            var tmin = Math.Max(Math.Max(xtmin, ytmin), ztmin);
+            var tmax = Math.Min(Math.Min(xtmax, ytmax), ztmax);
+
+            if (tmin > tmax) return new Intersection[] { };
+
+            return new Intersection[] { 
+                new Intersection(tmin, this), 
+                new Intersection(tmax, this)
+            };
+        }
+
+        protected override PointType LocalNormalAt(PointType point)
+        {
+            var maxC = Math.Max(Math.Max(Math.Abs(point.X), Math.Abs(point.Y)), Math.Abs(point.Z));
+
+            if(maxC == Math.Abs(point.X))
+                return PointType.Vector(point.X, 0, 0);
+            if (maxC == Math.Abs(point.Y))
+                return PointType.Vector(0, point.Y, 0);
+            if (maxC == Math.Abs(point.Z))
+                return PointType.Vector(0, 0, point.Z);
+
+            throw new Exception("No normal found");
+        }
+
+        private Tuple<double, double> CheckAxis(double origin, double direction)
+        {
+            var tMinNumerator = (-1 - origin);
+            var tMaxNumerator = (1 - origin);
+            
+            double tmax, tmin;
+            if (Math.Abs(direction) >= EPSILON)
+            {
+                tmin = tMinNumerator / direction;
+                tmax = tMaxNumerator / direction;
+            }
+            else
+            {
+                tmin = tMinNumerator * double.PositiveInfinity;
+                tmax = tMaxNumerator * double.PositiveInfinity;
+            }
+            if (tmin > tmax) return new Tuple<double, double>(tmax, tmin);
+            return new Tuple<double, double>(tmin, tmax);
+        }
+    }
+}
---
+++ Tests.RTC/Cubes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Tests.RTC.Helpers;
+using Xunit;
+using pt = RayTracerChallenge.Features.PointType;
+using shape = RayTracerChallenge.Features.Shapes;
+using pattern = RayTracerChallenge.Features.Patterns;
+using transform = RayTracerChallenge.Helpers.Transformations;
+using c = RayTracerChallenge.Features.Color;
+using RayTracerChallenge.Features;
+
+namespace Tests.RTC
+{
+    public class Cubes
+    {
+        [Theory]
+        [InlineData(5, 0.5, 0, -1, 0, 0, 4, 6)]
+        [InlineData(-5, 0.5, 0, 1, 0, 0, 4, 6)]
+        [InlineData(0.5, 5, 0, 0, -1, 0, 4, 6)]
+        [InlineData(0.5, -5, 0, 0, 1, 0, 4, 6)]
+        [InlineData(0.5, 0, 5, 0, 0, -1, 4, 6)]
+        [InlineData(0.5, 0, -5, 0, 0, 1, 4, 6)]
+        [InlineData(0, 0.5, 0, 0, 0, 1, -1, 1)]
+        public void RayIntersectCube(double px, double py, double pz, double vx, double vy, double vz, double t1, double t2)
+        {
+            var point = pt.Point(px, py, pz);
+            var direction = pt.Vector(vx, vy, vz);
+
+            var c = new shape.Cube();
+            var r = new Ray(point, direction);
+
+            var xs = c.Intersect(r);
+            Assert.Equal(2, xs.Length);
+            Assert.Equal(t1, xs[0].T);
+            Assert.Equal(t2, xs[1].T);
+        }
+
+        [Theory]
+        [InlineData(-2, 0, 0, 0.2673, 0.5343, 0.8018)]
+        [InlineData(0, -2, 0, 0.8018, 0.2673, 0.5345)]
+        [InlineData(0, 0, -2, 0.5345, 0.8018, 0.2673)]
+        [InlineData(2, 0, 2, 0, 0, -1)]
+        [InlineData(0, 2, 2, 0, -1, 0)]
+        [InlineData(2, 2, 0, -1, 0, 0)]
+        public void RayMissCube(double px, double py, double pz, double vx, double vy, double vz)
+        {
+            var point = pt.Point(px, py, pz);
+            var direction = pt.Vector(vx, vy, vz);
+
+            var c = new shape.Cube();
+            var r = new Ray(point, direction);
+
+            var xs = c.Intersect(r);
+            Assert.Empty(xs);
+        }
+
+        [Theory]
+        [InlineData(1, 0.5, -0.8, 1, 0, 0)]
+        [InlineData(-1, -0.2, 0.9, -1, 0, 0)]
+        [InlineData(-0.4, 1, -0.1, 0, 1, 0)]
+        [InlineData(0.3, -1, -0.7, 0, -1, 0)]
+        [InlineData(-0.6, 0.3, 1, 0, 0, 1)]
+        [InlineData(0.4, 0.4, -1, 0, 0, -1)]
+        [InlineData(1, 1, 1, 1, 0, 0)]
+        [InlineData(-1, -1, -1, -1, 0, 0)]
+        public void NormalSurfaceCube(double px, double py, double pz, double vx, double vy, double vz)
+        {
+            var p = pt.Point(px, py, pz);
+            var expNormal = pt.Vector(vx, vy, vz);
+
+            var c = new shape.Cube();
+            var normal = c.NormalAt(p);
+            Assert.Equal(expNormal, normal);
+        }
+
+    }
+}
